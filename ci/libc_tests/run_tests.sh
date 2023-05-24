eb542deb0ad05d632c74a953fbf2db5eecac651f
i.Pear <i.pear@outlook.com>
2023-05-24 17:46:08 +0800

    Update run_tests.sh
    Signed-off-by: i.Pear <i.pear@outlook.com>

--- ci/libc_tests/run_tests.sh
+++ ci/libc_tests/run_tests.sh
@@ -6,4 +6,4 @@ git clone https://github.com/unikraft/lib-libc-test
 cp config app-helloworld/.config
 cp Makefile app-helloworld/Makefile
 cd app-helloworld && make -j$(nproc)
-qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -cpu Skylake-Server > ../test_results.txt
+qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -cpu Skylake-Server
