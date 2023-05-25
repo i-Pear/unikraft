05d0f8235a0b959d3794b2e97a7e68644a7cc13e
i.Pear <i.pear@outlook.com>
2023-05-25 19:38:20 +0800

    Update run_tests.sh
    Signed-off-by: i.Pear <i.pear@outlook.com>

--- ci/libc_tests/run_tests.sh
+++ ci/libc_tests/run_tests.sh
@@ -3,7 +3,8 @@ sudo apt-get install -y qemu-system-x86-64 > /dev/null
 
 cd ci/libc_tests
 git clone https://github.com/unikraft/app-helloworld
-git clone https://github.com/unikraft/lib-libc-test
+git clone https://github.com/i-Pear/lib-libc-test
+git clone https://github.com/unikraft/lib-musl
 cp config app-helloworld/.config
 cp Makefile app-helloworld/Makefile
 cd app-helloworld && make -j$(nproc) > /dev/null
