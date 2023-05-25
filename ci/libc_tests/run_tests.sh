e02446faecf09dd9a87ae1b49e13b4213a6ca54d
i.Pear <i.pear@outlook.com>
2023-05-26 03:03:26 +0800

    Update run_tests.sh
    Signed-off-by: i.Pear <i.pear@outlook.com>

--- ci/libc_tests/run_tests.sh
+++ ci/libc_tests/run_tests.sh
@@ -5,6 +5,7 @@ cd ci/libc_tests
 git clone https://github.com/unikraft/app-helloworld > /dev/null 2>&1
 git clone https://github.com/i-Pear/lib-libc-test > /dev/null 2>&1
 git clone https://github.com/unikraft/lib-musl > /dev/null 2>&1
+git clone https://github.com/unikraft/lib-lwip > /dev/null 2>&1
 cp config app-helloworld/.config
 cp Makefile app-helloworld/Makefile
 cd app-helloworld && make -j$(nproc) > /dev/null 2>&1
