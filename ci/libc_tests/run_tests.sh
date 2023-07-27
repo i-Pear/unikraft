1aff862316ee2a5083202b6db592249114d107d3
i-Pear <i.pear@outlook.com>
2023-07-27 13:11:02 +0800

    1

--- ci/libc_tests/run_tests.sh
+++ ci/libc_tests/run_tests.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y > /dev/null 2>&1
-sudo apt-get install -y qemu-system-x86-64 > /dev/null 2>&1
+sudo apt-get install -y qemu-system-x86-64 python3 > /dev/null 2>&1
 
 cd ci/libc_tests
 git clone https://github.com/unikraft/app-helloworld > /dev/null 2>&1
@@ -9,4 +9,4 @@ git clone https://github.com/unikraft/lib-lwip > /dev/null 2>&1
 cp config app-helloworld/.config
 cp Makefile app-helloworld/Makefile
 cd app-helloworld && make -j$(nproc) > /dev/null 2>&1
-qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -enable-kvm -cpu host
+qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -cpu host
