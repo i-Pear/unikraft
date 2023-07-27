670b28ca644ed045e68856479b4d63fce08ef5c9
i-Pear <i.pear@outlook.com>
2023-07-27 14:07:53 +0800

    1

--- ci/libc_tests/run_tests.sh
+++ ci/libc_tests/run_tests.sh
@@ -1,12 +1,11 @@
 sudo apt-get update -y > /dev/null 2>&1
 sudo apt-get install -y qemu-system-x86-64 python3 > /dev/null 2>&1
 
-cd ci/libc_tests
-git clone https://github.com/unikraft/app-helloworld > /dev/null 2>&1
-git clone https://github.com/i-Pear/lib-libc-test > /dev/null 2>&1
-git clone https://github.com/unikraft/lib-musl > /dev/null 2>&1
-git clone https://github.com/unikraft/lib-lwip > /dev/null 2>&1
+git clone https://github.com/unikraft/app-helloworld --depth=1 > /dev/null 2>&1
+git clone https://github.com/i-Pear/lib-libc-test --depth=1 > /dev/null 2>&1
+git clone https://github.com/unikraft/lib-musl --depth=1 > /dev/null 2>&1
+git clone https://github.com/unikraft/lib-lwip --depth=1 > /dev/null 2>&1
 cp config app-helloworld/.config
 cp Makefile app-helloworld/Makefile
 cd app-helloworld && make -j$(nproc) > /dev/null 2>&1
-qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -cpu max
+qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -cpu max > test_result.txt
