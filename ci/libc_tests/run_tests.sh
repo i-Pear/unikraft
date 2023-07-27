e96b29b98eed51990c8467812a1ebdcc462b41a6
i-Pear <i.pear@outlook.com>
2023-07-27 12:57:02 +0800

    fix style

--- ci/libc_tests/run_tests.sh
+++ ci/libc_tests/run_tests.sh
@@ -9,4 +9,4 @@ git clone https://github.com/unikraft/lib-lwip > /dev/null 2>&1
 cp config app-helloworld/.config
 cp Makefile app-helloworld/Makefile
 cd app-helloworld && make -j$(nproc) > /dev/null 2>&1
-qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -cpu Skylake-Server
+qemu-system-x86_64 -kernel "build/app-helloworld_qemu-x86_64" -nographic -enable-kvm -cpu host
